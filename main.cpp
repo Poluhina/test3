eab202947f47e7ac56719e46038c3dc43e076d72
Poluhina <nastyapoluhina04@gmail.com>
2023-09-28 11:22:26 +0300

    Changed some stuff

--- main.cpp
+++ main.cpp
@@ -1,3 +1,30 @@
-int main{ 
+#include <iostream>
+
+int main ()
+{
+int i;
+
+i=69;
+
+int number =72
+
+i= 69+72;
+
+int a[3]={0,0,0};
+
+int* iPointer=&i;
+
+number= *iPointer;
+
+*iPointer=15;
+
+iPointer=&a[0];
+
+iPointer+=2;
+
+a[0]=6;
+*(iPointer+1)=6;
+
 return 0;
 }
+
